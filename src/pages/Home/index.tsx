7e3176603fc7b6631bbc32c1abf80d42b4c06c9b
miguelsndc <mglsndc@gmail.com>
2021-06-22 10:37:05 -0300

    feat: add home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import IllustrationImg from '../../assets/images/illustration.svg'
+import GoogleIconImg from '../../assets/images/google-icon.svg'
+import LogoImg from '../../assets/images/logo.svg'
+import EnterRoomIconImg from '../../assets/images/enter.svg'
+
+import { Button } from '../../components/Button/'
+
+import { PageAuth, MainContent, Separator, CreateRoomBtn } from './styles'
+
+export function Home() {
+  return (
+    <PageAuth>
+      <aside>
+        <img
+          src={IllustrationImg}
+          alt='Ilustração simbolizando perguntas e respostas'
+        />
+        <strong>Toda pergunta tem uma resposta.</strong>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main>
+        <MainContent>
+          <img src={LogoImg} alt='Letmeask' />
+          <CreateRoomBtn>
+            <img src={GoogleIconImg} alt='Logo do Google' />
+            Crie sua sala com o google
+          </CreateRoomBtn>
+          <Separator>ou entre em uma sala</Separator>
+          <form>
+            <input type='text' placeholder='Digite o código da sala' />
+            <Button type='submit'>
+              <img src={EnterRoomIconImg} alt='Entrar na sala' />
+              Entrar na sala
+            </Button>
+          </form>
+        </MainContent>
+      </main>
+    </PageAuth>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,132 @@
+import styled from 'styled-components'
+
+export const PageAuth = styled.div`
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+
+  aside {
+    flex: 7;
+
+    background: ${props => props.theme.purple};
+    color: ${props => props.theme.white};
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 7.5rem 5rem;
+
+    img {
+      max-width: 20rem;
+    }
+
+    strong {
+      font: 700 2.25rem 'Poppins', sans-serif;
+      line-height: 42px;
+      margin-top: 1rem;
+    }
+
+    p {
+      font-size: 1.5rem;
+      line-height: 2rem;
+      margin-top: 1rem;
+      opacity: 0.8;
+      color: ${props => props.theme.textDarker};
+    }
+  }
+  main {
+    flex: 8;
+
+    padding: 0 2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
+
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 20rem;
+  align-items: stretch;
+  text-align: center;
+
+  & > img {
+    align-self: center;
+  }
+
+  form {
+    input {
+      height: 3.125rem;
+      border-radius: 8px;
+      padding: 0 1rem;
+      background: ${props => props.theme.white};
+      border: 1px solid ${props => props.theme.grayMedium};
+    }
+
+    button {
+      margin-top: 1rem;
+    }
+
+    button,
+    input {
+      width: 100%;
+    }
+  }
+`
+
+export const CreateRoomBtn = styled.button`
+  margin-top: 4rem;
+  height: 3.125rem;
+  border-radius: 8px;
+  font-weight: 500;
+
+  background: ${props => props.theme.googleRed};
+  color: ${props => props.theme.white};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  border: none;
+
+  img {
+    margin-right: 8px;
+  }
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`
+
+export const Separator = styled.div`
+  font-size: 0.875rem;
+  color: ${props => props.theme.grayMedium};
+
+  margin: 2rem 0;
+
+  display: flex;
+  align-items: center;
+
+  &::before {
+    content: '';
+    flex: 1;
+    height: 1px;
+    background: ${props => props.theme.grayMedium};
+    margin-right: 1rem;
+  }
+  &::after {
+    content: '';
+    flex: 1;
+    height: 1px;
+    background: ${props => props.theme.grayMedium};
+    margin-left: 1rem;
+  }
+`

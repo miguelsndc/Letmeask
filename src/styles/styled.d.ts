49999d1848bdafe4eadaa3ab7fd902dd9be3eba6
miguelsndc <mglsndc@gmail.com>
2021-06-22 10:36:39 -0300

    chore: create theme type declaration file

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,18 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background: string
+    textDarker: string
+    text: string
+    purple: string
+    white: string
+    googleRed: string
+    pinkDark: string
+    danger: string
+    shadow: string
+    grayDark: string
+    grayMedium: string
+    grayLight: string
+  }
+}

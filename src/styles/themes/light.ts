5596720778937b3c1908ba847fbc719011543a65
miguelsndc <mglsndc@gmail.com>
2021-06-22 10:38:04 -0300

    feat: add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+const LightTheme = {
+  background: '#F8F8F8',
+  textDarker: '#f8f8f8',
+  text: '#29292E',
+  purple: '#835afd',
+  white: '#fff',
+  googleRed: '#EA4335',
+  pinkDark: '#E559F9',
+  danger: '#E73F5D',
+  shadow: '#050206',
+  grayDark: '#737380',
+  grayMedium: '#A8A8B3',
+  grayLight: '#DBDCDD',
+}
+
+export default LightTheme

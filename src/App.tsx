b13d609488989970021f68c95433ef14a25df510
miguelsndc <mglsndc@gmail.com>
2021-06-22 10:33:29 -0300

    feat: add theme provider & global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './services/firebase'
 
+import { Home } from './pages/Home/'
+import { GlobalStyles } from './styles/globals'
+import { ThemeProvider } from 'styled-components'
+import LightTheme from './styles/themes/light'
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
+  )
 }
 
 export default App

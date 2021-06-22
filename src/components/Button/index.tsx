0162c60f75260c6ff4384c10e7953af8785989ce
miguelsndc <mglsndc@gmail.com>
2021-06-22 10:34:53 -0300

    feat: add global reusable button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ButtonHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
+  return <S.Button {...props} />
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  margin-top: 4rem;
+  height: 3.125rem;
+  border-radius: 8px;
+  font-weight: 500;
+
+  background: ${props => props.theme.purple};
+  color: ${props => props.theme.white};
+  padding: 0 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+
+  border: none;
+
+  img {
+    margin-right: 8px;
+  }
+
+  transition: filter 0.2s;
+
+  &:not(:disabled):hover {
+    filter: brightness(0.9);
+  }
+
+  &:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`

0f8d5d2359aef1c9943a60461a7f2403ed814798
miguelsndc <mglsndc@gmail.com>
2021-06-23 08:45:06 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './services/firebase'
 
 import { Home } from './pages/Home/'
+import { NewRoom } from './pages/newRoom'
 import { GlobalStyles } from './styles/globals'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import LightTheme from './styles/themes/light'
 
 function App() {
   return (
     <ThemeProvider theme={LightTheme}>
       <GlobalStyles />
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/rooms/new' component={NewRoom} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   )
 }

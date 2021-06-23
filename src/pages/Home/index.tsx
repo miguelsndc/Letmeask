aa22a6b413afbb23aa6271eebb752f59c6f21b09
miguelsndc <mglsndc@gmail.com>
2021-06-23 09:45:18 -0300

    style: add spacing for better readability

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,6 +15,7 @@ import IllustrationImg from '../../assets/images/illustration.svg'
 import GoogleIconImg from '../../assets/images/google-icon.svg'
 import LogoImg from '../../assets/images/logo.svg'
 import EnterRoomIconImg from '../../assets/images/enter.svg'
+
 export function Home() {
   const history = useHistory()
   const { signInWithGoogle, user } = useAuth()
@@ -37,14 +38,18 @@ export function Home() {
         <strong>Toda pergunta tem uma resposta.</strong>
         <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
+
       <main>
         <MainContent>
           <img src={LogoImg} alt='Letmeask' />
+
           <CreateRoomBtn onClick={handleCreateRoom}>
             <img src={GoogleIconImg} alt='Logo do Google' />
             Crie sua sala com o google
           </CreateRoomBtn>
+
           <Separator>ou entre em uma sala</Separator>
+
           <form>
             <input type='text' placeholder='Digite o código da sala' />
             <Button type='submit'>

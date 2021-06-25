9ea9ff5e2642ca6c1234d129396b66532513f9cc
miguelsndc <mglsndc@gmail.com>
2021-06-25 10:23:06 -0300

    feat: pass isAnswered & isHighlighted as props

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -5,9 +5,13 @@ import { RoomCode } from '../../components/RoomCode'
 import { Question } from '../../components/Question'
 import { Button } from '../../components/Button'
 
+import { database } from '../../services/firebase'
+
 import { useRoom } from '../../hooks/useRoom'
 
 import LogoImg from '../../assets/images/logo.svg'
+import CheckImg from '../../assets/images/check.svg'
+import AnswerImg from '../../assets/images/answer.svg'
 import DeleteImg from '../../assets/images/delete.svg'
 
 import * as S from '../../styles/pages/Room'
@@ -73,7 +77,30 @@ export function AdminRoom() {
                   key={question.id}
                   content={question.content}
                   author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighlighted={question.isHighlighted}
                 >
+                  {question.isAnswered || (
+                    <>
+                      <button
+                        type='button'
+                        onClick={() =>
+                          handleCheckQuestionAsAnswered(question.id)
+                        }
+                >
+                        <img
+                          src={CheckImg}
+                          alt='Marcar pergunta como respondida'
+                        />
+                      </button>
+                      <button
+                        type='button'
+                        onClick={() => handleHighlightQuestion(question.id)}
+                      >
+                        <img src={AnswerImg} alt='Dar destaque a pergunta' />
+                      </button>
+                    </>
+                  )}
                   <button
                     type='button'
                     onClick={() => handleDeleteQuestion(question.id)}
